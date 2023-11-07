e41bbe97b40f0fb04a48c2621851c0e7f7423880
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-11-07 15:48:06 +0000

    Notes: routes.js

--- src/member/routes.js
+++ src/member/routes.js
@@ -4,9 +4,11 @@ const memberRouter = Router();
 const { addMember } = require("./controllers");
 
 //When initially creating family members upon registering
+//Needs protecting? or potentially use the json web token on logging in to assign relationship ids in which case wil be protected.
 memberRouter.post("/");
 
 // load homepage for that member once the other get route and click prompt has been fulfilled
+//once the family get request that includes members has been mapped into buttons this route will be part of a function that runs when said family is clicked?
 memberRouter.get("/getMemberDetails/:name");
 
 // change name of the member.
