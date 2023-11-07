9a0a932381f1b9ede63e994e57063afa8cd87205
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-11-07 15:39:46 +0000

    Setup: controllers-addMember established

--- src/member/routes.js
+++ src/member/routes.js
@@ -1,14 +1,13 @@
 const { Router } = require("express");
 const memberRouter = Router();
 
+const { addMember } = require("./controllers");
+
 //When initially creating family members upon registering
 memberRouter.post("/");
 
 // load homepage for that member once the other get route and click prompt has been fulfilled
 memberRouter.get("/getMemberDetails/:name");
-
-// This will map the users upon persistent login so they can click on their user
-memberRouter.get("/");
 
 // change name of the member.
 memberRouter.put("/changeName");
