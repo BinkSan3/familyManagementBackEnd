daa0e95e33eb8880a43e656ceeadca7e86cd0118
Joe Simpson <josephsimpson94@googlemail.com>
2023-11-07 16:48:23 +0000

    setup: added all applicable family routes with middleware in family routes.js

---
+++ src/family/routes.js
@@ -0,0 +1,40 @@
+const { Router } = require("express");
+const familyRouter = Router();
+
+const {
+  getAllFamilies,
+  getSingleFamily,
+  registerFamily,
+  loginFamily,
+  updateFamilyUsername,
+  updateFamilyPassword,
+  deleteFamily,
+} = require("./controllers");
+
+const { hashPass, comparePass, tokenCheck } = require("../middleware");
+
+// get all users, mainly used for testing on backend
+familyRouter.get("/", getAllFamilies);
+
+// get a family by username
+familyRouter.get("/search/family", getSingleFamily);
+
+// register user in the body
+familyRouter.post("/register", hashPass, registerFamily);
+
+// login user, no pass hash or token check
+familyRouter.post("/login", comparePass, loginFamily);
+
+// token check for persistent login
+familyRouter.get("/authCheck", tokenCheck, loginFamily);
+
+// update username
+familyRouter.put("/account/updateUsername", updateFamilyUsername);
+
+// update password
+familyRouter.put("/account/updatePassword", hashPass, updateFamilyPassword);
+
+// delete user
+familyRouter.delete("/account/delete", deleteFamily);
+
+module.exports = familyRouter;
