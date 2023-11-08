7f2f34940c1d25a61cc02af3c812366682efd56b
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-11-08 13:37:33 +0000

    Enhancement: deletemember route and controller established

--- src/member/controllers.js
+++ src/member/controllers.js
@@ -3,7 +3,14 @@ const jwt = require("jsonwebtoken");
 
 const addMember = async (req, res) => {
   try {
-    const result = await Member.create(req.body);
+    if (!req.verfication) {
+      throw new Error("Not signed in");
+    }
+    const MemberId = req.verfication.id;
+    const result = await Member.create({
+      MemberId: MemberId,
+      name: req.body.name,
+    });
 
     res.status(201).json({ message: "success", result });
   } catch (error) {
@@ -14,4 +21,25 @@ const addMember = async (req, res) => {
   }
 };
 
-module.exports = { addMember };
+const deleteMember = async (req, res) => {
+  try {
+    if (!req.verfication) {
+      throw new Error("Not signed in");
+    }
+    const result = await Member.destroy({
+      where: {
+        id: req.body.id,
+      },
+    });
+
+    const successResponse = {
+      message: "success",
+      result,
+    };
+    res.send(successResponse);
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+module.exports = { addMember, deleteMember };
