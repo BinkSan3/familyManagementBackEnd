675398315985da8f1bb8e14b98b55fed4f066ab7
Joe Simpson <josephsimpson94@googlemail.com>
2023-11-08 10:40:06 +0000

    Setup: added syncTables and Family model and router to server.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 require("./db/connection");
 const cors = require("cors");
 
+const Family = require("./family/model");
+const familyRouter = require("./family/routes");
+
 const port = process.env.PORT || 5001;
 
 const app = express();
@@ -16,10 +19,22 @@ app.use(
   })
 );
 
+app.use("/family", familyRouter);
+
+const syncTables = async () => {
+  await Family.sync();
+};
+
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "API is healthy!" });
 });
 
 app.listen(port, async () => {
+  try {
+    await syncTables();
+    console.log("Database tables synced succesfully!");
+  } catch (error) {
+    console.error("Error syncing tables.", error);
+  }
   console.log(`App is listening ${port}`);
 });
