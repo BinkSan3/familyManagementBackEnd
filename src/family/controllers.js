658faf6435b9185bd6bc03fc6ed6e752681cf062
Joe Simpson <josephsimpson94@googlemail.com>
2023-11-10 11:06:02 +0000

    bugfix: persistent login fully functional

--- src/family/controllers.js
+++ src/family/controllers.js
@@ -56,8 +56,6 @@ const registerFamily = async (req, res) => {
 //ASK MICHAEL about lazy loading vs eager loading and which function works better to get the members
 const loginFamily = async (req, res) => {
   try {
-    console.log("FROM LOGING", req.verification);
-
     if (req.family) {
       const token = await jwt.sign(
         { id: req.family.id },
@@ -77,9 +75,15 @@ const loginFamily = async (req, res) => {
       return;
     }
     if (req.verification) {
-      res
-        .status(200)
-        .json({ message: "Successful Login!", family: req.verification });
+      const members = await req.verification.getMembers();
+      res.status(200).json({
+        message: "Successful Login!",
+        family: {
+          username: req.verification.username,
+          email: req.verification.email,
+          members,
+        },
+      });
       return;
     }
   } catch (error) {
